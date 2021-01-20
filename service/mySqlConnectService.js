b666e5cd6ba7d3af906abf910443fc7c7341ac76
LegionCom <ma.Liguo@outlook.com>
2021-01-19 23:46:32 -0800

    heroku

--- service/mySqlConnectService.js
+++ service/mySqlConnectService.js
@@ -5,6 +5,7 @@ module.exports.connectDB=function () {
     const connection = mysql.createConnection(databaseOptions);
     connection.connect(function(err) {
         if (err) {
+            console.log('Connect errr to the MySQL server.', err);
             return console.error('error: ' + err.message);
         }
         console.log('Connected to the MySQL server.');
