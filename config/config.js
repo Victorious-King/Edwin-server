8ddc729e904d519a3c04444a85841a00e60cc611
LegionCom <ma.Liguo@outlook.com>
2021-01-19 20:34:59 -0800

    heroku

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var databaseOptions = {
-    host     : 'us-cdbr-east-03.cleardb.com',
-    database : 'heroku_cadf03bd7ab2e29',
-    user     : 'b33f45b7162c06',
-    password : 'f7fd423a'
+    host     : "us-cdbr-east-03.cleardb.com",
+    database : "heroku_cadf03bd7ab2e29",
+    user     : "b33f45b7162c06",
+    password : "f7fd423a"
 };
 module.exports = databaseOptions ;
